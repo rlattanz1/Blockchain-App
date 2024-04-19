d16c12e8508553006c7f526de8547a35187cff0c
Rocco Lattanzio <lukelattanzio@yahoo.com>
2024-04-19 15:59:55 -0700

    fixed old poll logice and added a new mapping to link user addresses to polls

--- Backend/contracts/Polling.sol
+++ Backend/contracts/Polling.sol
@@ -31,21 +31,24 @@ contract Polling {
     // creating an object of key value pairs for the Option structs
     mapping(uint => Poll) public polls;
 
-    function addOption (string memory _name) private {
-        optionId++;
-        options.push(Option(optionId, _name, 0));
-    }
+    mapping(address => Poll[]) private userPolls;
+
+    // function addOption (string memory _name) private {
+    //     optionId++;
+    //     Option(optionId, _name, 0);
+    // }
 
     function createPoll (string[] memory _options, string memory _name) public { //need a calldata/memory call in front of _options
         require( _options.length >= 2 && _options.length <= 10, "number of options must be between 2 and 10");
         pollId++;
 
         for (uint i = 0; i < _options.length; i++) {
-            addOption(_options[i]);
+            polls[pollId].options.push(Option(optionId++, _options[i], 0));
         }
 
-       polls[pollId] = Poll(pollId, _name, options);
-
+        polls[pollId].id = pollId;
+        polls[pollId].name = _name;
+        userPolls[msg.sender].push(polls[pollId]);
     }
 
 
