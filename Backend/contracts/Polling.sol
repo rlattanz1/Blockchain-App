815a2d02d2bf2137ddad3f15d2754fa665b4696e
Rocco Lattanzio <lukelattanzio@yahoo.com>
2024-04-19 13:54:25 -0700

    working on polling smart contract functions

--- Backend/contracts/Polling.sol
+++ Backend/contracts/Polling.sol
@@ -16,25 +16,35 @@ contract Polling {
         Option[] options;
     }
 
+    // storing the option id as a uint variable
     uint public optionId;
 
+    // storing the poll id as a uint variable
+    uint public pollId;
+
+
     // creating an object of key value pairs for the Option structs
-    // mapping(uint => Option) public Options;
+    // mapping(uint => Option) public options;
 
     Option[] public options;    // creating an array of the Option structs
 
-    function addOptions (string _name) private {
-        optionId ++;
+    // creating an object of key value pairs for the Option structs
+    mapping(uint => Poll) public polls;
+
+    function addOption (string _name) private {
+        optionId++;
         options.push(Option(optionId, _name, 0));
     }
 
-    function createPoll (string[] _options, string _name) public {
+    function createPoll (string[] memory _options, string memory _name) public returns (Poll memory) { //need a calldata/memory call in front of _options
         require( _options.length >= 2 && _options.length <= 10, "number of options must be between 2 and 10");
+        pollId++;
 
         for (uint i = 0; i < _options.length; i++) {
-            addOptions(_options[i]);
+            addOption(_options[i]);
         }
-        
+
+    `   polls[pollId] = Poll(pollId, _name, _options);
     }
 
 
