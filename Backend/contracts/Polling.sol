79c0e6b849d2d50d9f3128c9686a691b3248a108
Rocco Lattanzio <lukelattanzio@yahoo.com>
2024-04-19 16:22:21 -0700

    added group logic

--- Backend/contracts/Polling.sol
+++ Backend/contracts/Polling.sol
@@ -9,15 +9,22 @@ contract Polling {
     }
 
 
+    struct Group {
+        uint id;
+        string name;
+        address[] members;
+    }
+
 
     struct Poll {
         uint id;
         string name;
         Option[] options;
+        Group voters;
     }
 
     // storing the option id as a uint variable
-    uint public optionId;
+    // uint public optionId;
 
     // storing the poll id as a uint variable
     uint public pollId;
@@ -33,22 +40,23 @@ contract Polling {
 
     mapping(address => Poll[]) private userPolls;
 
-    // function addOption (string memory _name) private {
-    //     optionId++;
-    //     Option(optionId, _name, 0);
-    // }
+    mapping(uint => Group) private pollGroup
 
-    function createPoll (string[] memory _options, string memory _name) public { //need a calldata/memory call in front of _options
+
+    function createPoll (string[] memory _options, address[] memory _group, string memory _name) public { //need a calldata/memory call in front of _options
         require( _options.length >= 2 && _options.length <= 10, "number of options must be between 2 and 10");
+        require( _group.length >= 2, "must have at least 2 people for a poll");
+
         pollId++;
 
         for (uint i = 0; i < _options.length; i++) {
-            polls[pollId].options.push(Option(optionId++, _options[i], 0));
+            polls[pollId].options.push(Option(i+1, _options[i], 0));
         }
 
         polls[pollId].id = pollId;
         polls[pollId].name = _name;
         userPolls[msg.sender].push(polls[pollId]);
+        
     }
 
 
