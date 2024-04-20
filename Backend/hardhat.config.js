4b7c8a1aeda9079abf7a952fae00c648b12ce771
Rocco Lattanzio <lukelattanzio@yahoo.com>
2024-04-20 11:31:38 -0700

    uncommented out pollId

--- Backend/hardhat.config.js
+++ Backend/hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-ganache");
+require("@nomicfoundation/hardhat-ignition-ethers");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
