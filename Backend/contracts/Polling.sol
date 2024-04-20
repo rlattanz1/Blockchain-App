305ad0926dbe97e14149ca1570af050565faad9b
Rocco Lattanzio <lukelattanzio@yahoo.com>
2024-04-19 20:24:15 -0700

    started the vote function

--- Backend/contracts/Polling.sol
+++ Backend/contracts/Polling.sol
@@ -9,10 +9,10 @@ contract Polling {
     }
 
 
-    struct Group {
+    struct Voter {
         uint id;
         string name;
-        address[] members;
+        address member;
     }
 
 
@@ -20,15 +20,16 @@ contract Polling {
         uint id;
         string name;
         Option[] options;
-        Group[] voters;
+        Voter[] voters;
     }
 
     // storing the option id as a uint variable
     // uint public optionId;
 
     // storing the poll id as a uint variable
-    uint public pollId, groupId;
-    // uint public groupId;
+    uint public pollId;
+
+    uint public voterId;
 
 
     // creating an object of key value pairs for the Option structs
@@ -37,30 +38,33 @@ contract Polling {
     Option[] public options;    // creating an array of the Option structs
 
     // creating an object of key value pairs for the Option structs
-    mapping(uint => Poll) public polls;
+    mapping(uint256 => Poll) public polls;
 
-    mapping(address => Poll[]) private userPolls;
+    mapping(address => Poll[]) public userPolls;
 
-    mapping(uint => Group) private pollGroup;
+    mapping(uint256 => Voter[]) public pollGroup;
+
+    mapping(uint256 => mapping(address => bool)) public voters;
 
 
-    function createPoll (string[] memory _options, address[] memory _group, string memory _name) public { //need a calldata/memory call in front of _options
+
+    function createPoll (string[] memory _options, address[] memory _group, string memory _pollName) public { //need a calldata/memory call in front of _options
         require( _options.length >= 2 && _options.length <= 10, "number of options must be between 2 and 10");
         require( _group.length >= 2, "must have at least 2 people for a poll");
 
         pollId++;
-        groupId++;
+
         for (uint i = 0; i < _options.length; i++) {
             polls[pollId].options.push(Option(i+1, _options[i], 0));
         }
 
-        for (uint i = 0; i < _group.length; i++) {
-            polls[pollId].voters.push(Group());
-        }
-
         polls[pollId].id = pollId;
-        polls[pollId].name = _name;
+        polls[pollId].name = _pollName;
         userPolls[msg.sender].push(polls[pollId]);
+    }
+
+    function Vote () public {
+        
     }
 
 
