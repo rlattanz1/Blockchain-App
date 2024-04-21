81b16ec26d66c5b10dcf0e6a5f57be0f261bd4a5
Brandon <b.n.tamayo7@gmail.com>
2024-04-20 19:37:11 -0700

    Uninstalled tailwind and started the header and poll display components for the home page

--- Backend/hardhat.config.js
+++ Backend/hardhat.config.js
@@ -5,4 +5,12 @@ require("@nomicfoundation/hardhat-ignition-ethers");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  defaultNetwork: "localhost",
+  networks: {
+    hardhat: {
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+  },
 };
